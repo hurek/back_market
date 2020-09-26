d5937d62a9771a54934e32526c6f0d6f74495374
hurek <61438668+hurek@users.noreply.github.com>
2020-09-26 06:20:35 +0300

    lastest

---
+++ config.py
@@ -0,0 +1,5 @@
+import telebot
+
+def initialize():
+	bot = telebot.TeleBot('1370239055:AAGCrr6VecME59HQYZJRCdGaMLkVJYXb52g')
+	return bot
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sqlite3
 import telebot
 import time
 from telebot import types
+from config import initialize
 
-bot = telebot.TeleBot("1312594008:AAEcU-eiJJ_r1d57mA4Wnha3Kv14qf3cVsk")
+bot = initialize()
 
 
 def init():
@@ -15,16 +16,27 @@ def init():
                    """)
 
 	cursor.execute("""CREATE TABLE if not exists wallet
-                        (telegram_id number, balance number)
+                        (telegram_id number, username text, balance number default 0)
                     """)
+	cursor.execute("""CREATE TABLE if not exists steps
+	                        (telegram_id number, username text, step text default 'new', status text default 'wait')
+	                    """)
 	conn.close()
 
+def bd_change(query):
+	try:
+		conn = sqlite3.connect("mydatabase.db")
+		cursor = conn.cursor()
+		cursor.execute(query)
+		conn.commit()
+		conn.close()
+		return 0
+	except:
+		conn.close()
+	return 1
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-	conn = sqlite3.connect("mydatabase.db")
-	cursor = conn.cursor()
-	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{call.from_user.id}'""").fetchall()[0][1]
 	print(call)
 	if call.data == f'catalog{call.data[-1:]}':
 		bot.edit_message_reply_markup(chat_id=call.message.chat.id,
@@ -36,10 +48,6 @@ def callback_query(call):
 		bot.delete_message(call.message.chat.id, call.message.message_id)
 	elif call.data == 'null':
 		pass
-	elif call.data == 'Yes':
-			job(call)
-	elif call.data == 'No':
-		pass
 	else:
 		send_product(call.data[:-1], call.message.chat)
 
@@ -49,13 +57,13 @@ def send_welcome(message):
 	conn = sqlite3.connect("mydatabase.db")
 	cursor = conn.cursor()
 	if not cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = {message.from_user.id}""").fetchall():
-		cursor.execute(f"""INSERT INTO wallet VALUES ({message.from_user.id}, 0)""")
+		cursor.execute(f"""INSERT INTO wallet ('telegram_id', 'username') VALUES ('{message.from_user.id}', '{message.from_user.username}')""")
+		cursor.execute(f"""INSERT INTO steps ('telegram_id', 'username') VALUES ('{message.from_user.id}', '{message.from_user.username}')""")
 		conn.commit()
 	keyboard = types.InlineKeyboardMarkup()
 	keyboard.add(types.InlineKeyboardButton(text="Catalog", callback_data='catalog0'))
 	bot.send_photo(message.from_user.id, open('img/blackmarket.png', 'rb'), reply_markup=keyboard)
 	conn.close()
-
 
 def catalog_keyboard(page):
 	keyboard = types.InlineKeyboardMarkup()
@@ -102,153 +110,101 @@ def send_product(product, chat):
 def buy_product(call):
 	conn = sqlite3.connect("mydatabase.db")
 	cursor = conn.cursor()
-	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{call.from_user.id}'""").fetchall()[0][1]
-	price = cursor.execute(f"""SELECT * FROM shop WHERE name = '{call.data[3:]}'""").fetchall()[0][3]
+	balance = cursor.execute(f"SELECT * FROM wallet WHERE telegram_id = '{call.from_user.id}'").fetchall()[0][2]
+	price = cursor.execute(f"SELECT * FROM shop WHERE name = '{call.data[3:]}'").fetchall()[0][3]
 	if balance >= price:
 		cursor.execute(f"""UPDATE wallet SET balance = {balance - price} WHERE telegram_id = {call.from_user.id}""")
 		conn.commit()
-		bot.send_message(call.message.chat.id, f"Grats. You bought! {call.data[3:]}")
+		conn.close()
+		bot.send_message(call.message.chat.id, f"Grats. You bought {call.data[3:]}!\nYour order number:\n3cb50a7b990798c0559b9032b5f8586f7de223217528591d6f3dce959180038b")
 	else:
+		conn.close()
 		bot.send_message(call.message.chat.id, "Sorry, you don't have enough money.")
 		time.sleep(2)
-		# order(call)
-		bot.send_message(call.message.chat.id, "Do you need job?")
+		bot.send_message(call.message.chat.id, "If you need money, I have an offer for you. Write me /job and I'll tell you more.")
 
+@bot.message_handler(commands=['job'])
+def job(message):
+	conn = sqlite3.connect("mydatabase.db")
+	cursor = conn.cursor()
+	task = {
+		1: {'text': "This is your first task:\nhttps://imgur.com/a/IuepuX6\nIn addition to selling unusual items, I also do detective work.\nOne of my clients asked me to decipher it.\nSend me an answer when you're done.",'answer': "minimumstakeamount"},
+		2: {'text': "This is your second task:\nhttps://pastebin.com/MjfZN9t5\nAnother client encrypted the password to his bank account and forgot how to decrypt it.\nHe pays a lot of money for decryption. I'll wait for the password from the bank.",'answer': "1c62590c99871030411c60c4780da31ec05c4db349e2f02d79c01228fccd9380"},
+		3: {'text': "This is your third task:\n((BV80605001911AP)- (sqrt(-1)))^2\nAnd finally the last task. To be honest, I'm ashamed to ask you, but could you help with my child's homework?\nI've seen this problem somewhere, but I can't remember the solution.\nI'll wait for the number.",'answer': "-16"}}
+	if not cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = {message.from_user.id}""").fetchall():
+		cursor.execute(f"""INSERT INTO wallet ('telegram_id', 'username') VALUES ('{message.from_user.id}', '{message.from_user.username}')""")
+		cursor.execute(f"""INSERT INTO steps ('telegram_id', 'username') VALUES ('{message.from_user.id}', '{message.from_user.username}')""")
+		conn.commit()
+		conn.close()
+		return send_welcome(message)
+	step = cursor.execute(f"""SELECT step FROM steps WHERE telegram_id = '{message.from_user.id}'""").fetchone()[0]
+	if step == 'new':
+		bd_change(f"""UPDATE steps SET step = 'first' WHERE telegram_id = {message.from_user.id}""")
+	if step == 'first':
+		bot.send_message(message.chat.id, task[1]['text'])
+		bd_change(f"""UPDATE steps SET status = 'work' WHERE telegram_id = {message.from_user.id}""")
+	elif step == 'second':
+		bot.send_message(message.chat.id, task[2]['text'])
+		bd_change(f"""UPDATE steps SET status = 'work' WHERE telegram_id = {message.from_user.id}""")
+	elif step == 'third':
+		bot.send_message(message.chat.id, task[3]['text'])
+		bd_change(f"""UPDATE steps SET status = 'work' WHERE telegram_id = {message.from_user.id}""")
+	elif step == 'passed':
+		bot.send_message(message.chat.id, 'You already solved all my tasks! You can start trading now')
+	conn.close()
 
-# def order(call):
-# 	keyboard = types.InlineKeyboardMarkup()
-# 	keyboard.row(types.InlineKeyboardButton(text="Yes", callback_data="Yes"),
-# 				 types.InlineKeyboardButton(text="No", callback_data="No"))
-# 	bot.send_message(call.message.chat.id, f"Do you want to make money?", reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def quest1(message):
+def quest(message):
 	conn = sqlite3.connect("mydatabase.db")
 	cursor = conn.cursor()
-	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{message.from_user.id}'""").fetchall()[0][1]
-	chat_id = message.chat.id
-	if message.text == 'Yes' or 'yes' or 'yep':
-		if balance == 0:
-			bot.register_next_step_handler(bot.send_message(message.chat.id, 'It is your first task'), quest1)
-		elif balance == 2500:
-			bot.register_next_step_handler(bot.send_message(chat_id, "It is your second task"), quest2)
-		elif balance == 5500:
-			bot.register_next_step_handler(bot.send_message(chat_id, "It is your last task"), quest3)
-	else:
-		bot.send_message(chat_id, "You already done all job")
-
-def quest1(message):
-	conn = sqlite3.connect("mydatabase.db")
-	cursor = conn.cursor()
-	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{message.from_user.id}'""").fetchall()[0][1]
-	text = message.text
-	chat_id = message.chat.id
-	if text == "Answer1":
-		time.sleep(3)
-		cursor.execute(f"""UPDATE wallet SET balance = {balance + 2500} WHERE telegram_id = {message.from_user.id}""")
+	task = {
+		1: {'text': "This is your first task:\nhttps://imgur.com/a/IuepuX6\nIn addition to selling unusual items, I also do detective work.\nOne of my clients asked me to decipher it.\nSend me an answer when you're done.",'answer': "minimumstakeamount"},
+		2: {'text': "This is your second task:\nhttps://pastebin.com/MjfZN9t5\nAnother client encrypted the password to his bank account and forgot how to decrypt it.\nHe pays a lot of money for decryption. I'll wait for the password from the bank.",'answer': "1c62590c99871030411c60c4780da31ec05c4db349e2f02d79c01228fccd9380"},
+		3: {'text': "This is your third task:\n((BV80605001911AP)- (sqrt(-1)))^2\nAnd finally the last task. To be honest, I'm ashamed to ask you, but could you help with my child's homework?\nI've seen this problem somewhere, but I can't remember the solution.\nI'll wait for the number.",'answer': "-16"}}
+	if not cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = {message.from_user.id}""").fetchall():
+		cursor.execute(f"""INSERT INTO wallet ('telegram_id', 'username') VALUES ('{message.from_user.id}', '{message.from_user.username}')""")
+		cursor.execute(f"""INSERT INTO steps ('telegram_id', 'username') VALUES ('{message.from_user.id}', '{message.from_user.username}')""")
 		conn.commit()
-		msg =
-		bot.register_next_step_handler(bot.send_message(chat_id, f"Well done, take 2500$ \nThis is the second task:"), quest2)
-	else:
-		message = bot.send_message(chat_id, 'It\'s wrong. Come back when you finish task')
-		bot.register_next_step_handler(message, quest1)
+		conn.close()
+		return send_welcome(message)
+	step = cursor.execute(f"""SELECT step FROM steps WHERE telegram_id = '{message.from_user.id}'""").fetchone()[0]
+	status = cursor.execute(f"""SELECT status FROM steps WHERE telegram_id = '{message.from_user.id}'""").fetchone()[0]
+	if step == 'new':
 		return
-
-def quest2(message):
-	conn = sqlite3.connect("mydatabase.db")
-	cursor = conn.cursor()
+	if status == 'wait':
+		return
 	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{message.from_user.id}'""").fetchall()[0][1]
-	text = message.text
 	chat_id = message.chat.id
-	if text == "Answer2":
-		time.sleep(3)
-		cursor.execute(f"""UPDATE wallet SET balance = {balance + 3000} WHERE telegram_id = {message.from_user.id}""")
-		conn.commit()
-		bot.register_next_step_handler(bot.send_message(chat_id, f"Well done, take 3000$ \nThis is the last task:"), quest3)
+	if step == 'first':
+		if message.text == task[1]['answer']:
+			bd_change(f"""UPDATE steps SET step = 'second' WHERE telegram_id = {message.from_user.id}""")
+			bd_change(f"""UPDATE steps SET status = 'wait' WHERE telegram_id = {message.from_user.id}""")
+			bd_change(f"""UPDATE wallet SET balance = 3000 WHERE telegram_id = {message.from_user.id}""")
+			bot.send_message(message.chat.id, 'Well done! Take 3000$ Write me /job to get the next task.')
+		else:
+			bot.send_message(message.chat.id, 'Wrong answer! I\'m waiting for right answer.')
+	elif step == 'second':
+		if message.text == task[2]['answer']:
+			bd_change(f"""UPDATE steps SET step = 'third' WHERE telegram_id = {message.from_user.id}""")
+			bd_change(f"""UPDATE steps SET status = 'wait' WHERE telegram_id = {message.from_user.id}""")
+			bd_change(f"""UPDATE wallet SET balance = 6000 WHERE telegram_id = {message.from_user.id}""")
+			bot.send_message(message.chat.id, 'Well done! Take 3000$ Write me /job to get the next task.')
+		else:
+			bot.send_message(message.chat.id, 'Wrong answer! I\'m waiting for right answer.')
+	elif step == 'third':
+		if message.text == task[3]['answer']:
+			bd_change(f"""UPDATE steps SET step = 'passed' WHERE telegram_id = {message.from_user.id}""")
+			bd_change(f"""UPDATE wallet SET balance = 9000 WHERE telegram_id = {message.from_user.id}""")
+			bd_change(f"""UPDATE steps SET status = 'wait' WHERE telegram_id = {message.from_user.id}""")
+			bot.send_message(message.chat.id, 'Well done! Take 3000$ You\'re good! You completed all my tasks. You can start trading.')
+			send_welcome(message)
+		else:
+			bot.send_message(message.chat.id, 'Wrong answer! I\'m waiting for right answer.')
 	else:
-		message = bot.send_message(chat_id, 'It\'s wrong. Come back when you finish task')
-		bot.register_next_step_handler(message, quest1)
-		return
-
-def quest3(message):
-	conn = sqlite3.connect("mydatabase.db")
-	cursor = conn.cursor()
-	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{message.from_user.id}'""").fetchall()[0][1]
-	text = message.text
-	chat_id = message.chat.id
-	if text == "Answer3":
-		time.sleep(3)
-		cursor.execute(f"""UPDATE wallet SET balance = {balance + 3500} WHERE telegram_id = {message.from_user.id}""")
-		conn.commit()
-		bot.register_next_step_handler(bot.send_message(chat_id, f"I havent any work for you. You can \\start trading"), quest1)
-	else:
-		message = bot.send_message(chat_id, 'It\'s wrong. Come back when you finish task')
-		bot.register_next_step_handler(message, quest1)
-		return
-
-# def job(call):
-# 	conn = sqlite3.connect("mydatabase.db")
-# 	cursor = conn.cursor()
-# 	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{call.from_user.id}'""").fetchall()[0][1]
-#
-# def quest3(message):
-# 	conn = sqlite3.connect("mydatabase.db")
-# 	cursor = conn.cursor()
-# 	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{message.from_user.id}'""").fetchall()[0][1]
-# 	text = message.text
-# 	chat_id = message.chat.id
-# 	if text == "Answer1":
-# 		time.sleep(3)
-# 		bot.send_message(chat_id, f"Well done, take 2500$")
-# 		cursor.execute(f"""UPDATE wallet SET balance = {balance + 2500} WHERE telegram_id = {message.from_user.id}""")
-# 		conn.commit()
-# 	else:
-# 		message = bot.send_message(chat_id, 'It\'s wrong. Come back when you finish task')
-# 		bot.register_next_step_handler(message, quest1)
-# 		return
-
-# def job2(call):
-# 	conn = sqlite3.connect("mydatabase.db")
-# 	cursor = conn.cursor()
-# 	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{call.from_user.id}'""").fetchall()[0][1]
-# 	bot.send_message(call.message.chat.id, f"Second quest")
-# 	@bot.message_handler(func=lambda message: True, content_types=['text'])
-# 	def second_quest(message):
-# 		if message.text == "Answer2":
-# 			time.sleep(3)
-# 			bot.send_message(call.message.chat.id, f"Well done, take 3000$")
-# 			cursor.execute(f"""UPDATE wallet SET balance = {balance + 3000} WHERE telegram_id = {call.from_user.id}""")
-# 			conn.commit()
-# 		else:
-# 			bot.send_message(call.message.chat.id, f"Wrong answer 2!")
-#
-# def job3(call):
-# 	conn = sqlite3.connect("mydatabase.db")
-# 	cursor = conn.cursor()
-# 	balance = cursor.execute(f"""SELECT * FROM wallet WHERE telegram_id = '{call.from_user.id}'""").fetchall()[0][1]
-# 	bot.send_message(call.message.chat.id, f"Third quest")
-# 	@bot.message_handler(func=lambda message: True, content_types=['text'])
-# 	def third_quest(message):
-# 		if message.text == "Answer3":
-# 			time.sleep(3)
-# 			bot.send_message(call.message.chat.id, f"Well done, take 3500$")
-# 			cursor.execute(f"""UPDATE wallet SET balance = {balance + 3500} WHERE telegram_id = {call.from_user.id}""")
-# 			conn.commit()
-# 		else:
-# 			bot.send_message(call.message.chat.id, f"Wrong answer 3!")
-
-# @bot.message_handler(func=lambda message: message.text == 'I want to make money')
-# def quest1(message):
-# 	# conn = sqlite3.connect("mydatabase.db")
-# 	# cursor = conn.cursor()
-# 	# balance = 0
-# 	if balance == 0:
-# 		bot.send_message(message.chat.id, 'Task1')
-# 		bot
-# 	elif balance == 2500:
-# 		bot.send_message(message.chat.id, 'Task2')
-# 	elif balance == 5500:
-# 		bot.send_message(message.chat.id, 'Task3')
+		pass
+	conn.commit()
+	conn.close()
 
 init()
 bot.polling()
