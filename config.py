9a78f7ab1f2fefd155dfa196566f279f5ae70bc0
hurek <rudrobinrood@gmail.com>
2021-06-30 02:04:10 +0300

    исправлен токен

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import telebot
 
 def initialize():
-	bot = telebot.TeleBot('1370239055:AAGCrr6VecME59HQYZJRCdGaMLkVJYXb52g')
+	bot = telebot.TeleBot('1370239055:AAFs95ELoRUMZRWnrnV1zzInO83NjrXU_N0')
 	return bot
